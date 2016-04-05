b4b385bf3fdf2f3d2b6cf30a8a249c1875fa74a3
Carlos Lopez <genetita@gmail.com>
2016-04-05 14:39:14 +0200

    Remove tool functionality

--- Digit.gd
+++ Digit.gd
@@ -1,22 +1,19 @@
-tool
+
 extends GridContainer
 
 const led_size=480
 const grid_width=5
 const grid_height=7
 
-export var grid_width_pixels=32 setget set_grid_width
-
-func set_grid_width(new_width):
-	grid_width_pixels=new_width
-	var grid_scale=float(grid_width_pixels)/grid_width/led_size
-	set_scale(Vector2(grid_scale, grid_scale))
-	
+var grid_width_pixels=32
 
 func _ready():
-	set_grid_width(128)
 	parse_number(0)
-	pass
+
+func get_scale_for_width(var width_pixels):
+	var scale
+	scale=float(width_pixels)/led_size/grid_width
+	return scale
 
 func parse_number(var n):
 	var node=get_node("dictionary")
